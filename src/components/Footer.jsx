1800742150108ff49be88c4058b6745decc0b11f
Alberte <alan0002@stud.kea.dk>
2024-12-05 11:29:01 +0100

    footer is done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,18 +4,20 @@ import { FaTwitterSquare } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <section className=" h-500 flex justify-evenly p-10 fixed bottom-0 left-0 w-full">
+    <section className=" h-300 flex justify-evenly p-10 fixed bottom-0 w-full bg-white bg-opacity-30">
       <div>
         <FaInstagramSquare className="text-lilla h-50 w-50" />
         <FaFacebookSquare className="text-lilla h-50 w-50" />
         <FaTwitterSquare className="text-lilla h-50 w-50" />
       </div>
       <div className="text-center font-bold">
-        <p className="pb-5 text-lilla">Contact</p>
-        <p className="pb-5 text-lilla">Bla</p>
-        <p className="pb-5 text-lilla">Bla</p>
+        <p className="pb-5 pt-5 text-lilla text-[20px]">Contact</p>
+        <p className="pb-5 text-lilla text-[20px]">About Us</p>
+        <p className="pb-5 text-lilla text-[20px]">Sponsor</p>
       </div>
-      <div>Logo skal være her</div>
+      <div>
+        <img className="w-150 h-150" src="/img/logo.svg" alt="logo ikon"></img>
+      </div>
     </section>
   );
 };
