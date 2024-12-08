d7befa29187248c01f19efed539bfe3bccf01e2c
Alberte <alan0002@stud.kea.dk>
2024-12-08 16:14:17 +0100

    hej

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,21 @@
 import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
-
 import Arrow from "@/components/Arrow";
 import Banner from "@/components/Banner";
 import Image from "next/image";
 import Button from "@/components/Button";
 import Formel from "@/components/Formel";
+import Carrousel from "@/components/Carrousel";
 
 export default function Home() {
   return (
     <div>
+      {/*       <Carrousel /> */}
       <Nav />
       <Arrow />
-
-      <Nav></Nav>
       <Banner />
       <Button />
       <Formel />
-
       <Footer />
     </div>
   );
--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -5,7 +5,7 @@ import "swiper/css";
 import { FaArrowCircleRight, FaArrowCircleLeft } from "react-icons/fa";
 import Image from "next/image";
 
-const Carousel = () => {
+const Carrousel = () => {
   const [bands, setBands] = useState([]); // State til at gemme bands-data
 
   // Funktion til at hente data fra API
@@ -15,15 +15,11 @@ const Carousel = () => {
       const data = await response.json();
       setBands(data);
     };
-
-  },);
+  });
 
   return (
     <div className="relative">
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-      >
+      <Swiper spaceBetween={50} slidesPerView={1}>
         {bands.map((band) => (
           <SwiperSlide key={band.name}>
             <div className="bg-purple-500 h-64 flex flex-col items-center justify-center text-white">
@@ -40,7 +36,7 @@ const Carousel = () => {
         <FaArrowCircleLeft />
       </button>
       <button
-        onClick={() => swiperRef.current?.slideNext()} 
+        onClick={() => swiperRef.current?.slideNext()}
         className="absolute top-1/2 right-4 transform -translate-y-1/2 text-white text-3xl z-10"
       >
         <FaArrowCircleRight />
@@ -49,4 +45,4 @@ const Carousel = () => {
   );
 };
 
-export default Carousel;
+export default Carrousel;
