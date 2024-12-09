b44ee9e182f9132cef5cddad45911ce575dc431b
Alberte <alan0002@stud.kea.dk>
2024-12-09 10:12:08 +0100

    .

--- src/app/page.js
+++ src/app/page.js
@@ -6,16 +6,17 @@ import Image from "next/image";
 import Button from "@/components/Button";
 import Formel from "@/components/Formel";
 import Carrousel from "@/components/Carrousel";
-import Kurv from "@/components/Kurv";
-import Ticket from "@/components/Ticket";
 
 export default function Home() {
   return (
     <div>
-      {/*      <Formel /> */}
-      <Carrousel />
-      {/*       <Kurv />
-      <Ticket /> */}
+      {/*       <Carrousel /> */}
+      <Nav />
+      <Arrow />
+      <Banner />
+      <Button />
+      <Formel />
+      <Footer />
     </div>
   );
 }
