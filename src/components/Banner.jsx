ee00c9406b2273f4e266376c11bb8670ded5e57f
Catharina <catharina2002@gmail.com>
2024-12-17 12:46:59 +0100

    margin

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 const Banner = () => {
   return (
-    <div className="list-none flex animate-motion whitespace-nowrap">
+    <div className="list-none flex animate-motion whitespace-nowrap mt-10 mb-10">
       <li className="ml-3 font-bold">FooFest</li>
       <li className="ml-3 font-bold">-</li>
       <li className="ml-3 font-bold">25.09.2025-30.09.2025</li>
